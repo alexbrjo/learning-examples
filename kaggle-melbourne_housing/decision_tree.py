00c890d1ba4a799ee86eb49c96f7d94d3ddd969a
Alex Johnson <alexjo5000@gmail.com>
2022-07-24 09:58:28 -0700

    decision tree with train/test split

--- kaggle-melbourne_housing/decision_tree.py
+++ kaggle-melbourne_housing/decision_tree.py
@@ -1,28 +1,40 @@
 # Loosely following from https://www.kaggle.com/code/dansbecker/basic-data-exploration/tutorial
 
+# Suggested features
+# MAPE = 0.23646279738719264 for 
+# Features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+#
+# All Numeric features
+# MAPE = 0.0013906121511715005 for
+# Features = ['Rooms', 'Price', 'Distance', 'Postcode', 'Bedroom2', 'Bathroom', 'Car',
+#      'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude', 'Propertycount']
+
+
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split
 
 # dataset from https://www.kaggle.com/datasets/dansbecker/melbourne-housing-snapshot
 melbourne_file_path = '../dataset/melb_data.csv'
 
+# load and clean (drop na and non-numeric columns lol) data
 melbourne_data = pd.read_csv(melbourne_file_path)
 melbourne_data = melbourne_data.dropna()
 
-print(melbourne_data.describe())
-
-print(melbourne_data.columns)
-
 y = melbourne_data['Price']
-feature_names = melbourne_data[['Rooms', 'Price', 'Distance', 'Postcode', 'Bedroom2', 'Bathroom', 'Car',
+X = melbourne_data[['Rooms', 'Price', 'Distance', 'Postcode', 'Bedroom2', 'Bathroom', 'Car',
        'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude', 'Propertycount']]
-X = feature_names
+
+# train
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
 
 melbourne_model = DecisionTreeRegressor(random_state=0)
-melbourne_model.fit(X, y)
+melbourne_model.fit(train_X, train_y)
 
-predictions = melbourne_model.predict(X)
+# predict
+predictions = melbourne_model.predict(val_X)
 
-print(melbourne_data.head())
-print(predictions[:10])
+print("Features: ")
+print(X.columns)
+print("DecisionTreeRegressor MAPE: " + str(mean_absolute_percentage_error(val_y, predictions)))
