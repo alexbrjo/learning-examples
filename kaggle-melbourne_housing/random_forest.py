607e2e8c0752bf3b27a65888c8cdfaa82b1abb6e
Alex Johnson <alexjo5000@gmail.com>
2022-07-24 10:21:48 -0700

    random forest

--- kaggle-melbourne_housing/random_forest.py
+++ kaggle-melbourne_housing/random_forest.py
@@ -1,19 +1,12 @@
 # Loosely following from https://www.kaggle.com/code/dansbecker/basic-data-exploration/tutorial
 
-# Suggested features
-# MAPE = 0.23646279738719264 for 
-# Features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
-#
-# All Numeric features
-# MAPE = 0.0013906121511715005 for
-# Features = ['Rooms', 'Price', 'Distance', 'Postcode', 'Bedroom2', 'Bathroom', 'Car',
-#      'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude', 'Propertycount']
-#
-# Trying to find optimal MAE
+# Best decision tree
 # max_leaf_nodes: 1400 Features: 13 DecisionTreeRegressor MAPE: 0.0014673169224195199
+#
+
 
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
@@ -34,15 +27,13 @@ X = melbourne_data[['Rooms', 'Price', 'Distance', 'Postcode', 'Bedroom2', 'Bathr
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
 
 # predict
-def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    melbourne_model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+def get_mae(train_X, val_X, train_y, val_y):
+    melbourne_model = RandomForestRegressor(random_state=0)
     melbourne_model.fit(train_X, train_y)
     predictions = melbourne_model.predict(val_X)
     mae = mean_absolute_error(val_y, predictions)
-    print("max_leaf_nodes: " + str(max_leaf_nodes) + 
-        " Features: " + str(len(X.columns)) + 
+    print("Features: " + str(len(X.columns)) + 
         " DecisionTreeRegressor MAPE: " + str(mean_absolute_percentage_error(val_y, predictions)))
     return(mae)
 
-for max_leaf_nodes in range(100, 1000, 100):
-    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+get_mae(train_X, val_X, train_y, val_y)
